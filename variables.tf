e6c4206bf7b3aa24319c4a6da0ad5370b69e4ed9
Abbes-Younes <younes.abbes.work@gmail.com>
2026-04-23 23:47:52 +0100

    switched the password to its original form

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "db_user" {
 variable "db_password" {
   description = "Mot de passe PostgreSQL (ATTENTION: Simulé, ne pas utiliser en Prod!)"
   type        = string
-  default     = "strongpassword12345"
+  default     = "strongpassword123"
 }
 
 #--- Variables d'Application
